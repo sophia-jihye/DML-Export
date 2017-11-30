c81a5a232403c877495efd9d4ac6886b5f58c24d
jihye <jihye@13GRAM-i5-JIHYE>
2017-12-01 00:47:43 +0900

    modified sample test case

--- src/sophia2/liveUpdate/TestMain.java
+++ src/sophia2/liveUpdate/TestMain.java
@@ -7,33 +7,32 @@ public class TestMain {
 
 		String delimeter = ".";
 
-		Boolean majorUpdate = true;
+		Boolean majorUpdate = false;
 		String major = null;
 		String minor = null;
 
-		VersionDto versionDto = new VersionDto("1.19");
+		VersionDto versionDto = new VersionDto("9.9");
 		String version = versionDto.getVersion();
 		if (version == null) {
-			versionDto.setVersion("0.0");
+			versionDto.setVersion("1.0");
 		} else {
 			System.out.println(version);
 			major = version.substring(0, version.indexOf(delimeter));
 			System.out.println("major: " + major);
 			minor = version.substring(version.indexOf(delimeter) + 1);
 			System.out.println("minor: " + minor);
+
+			Integer majorInt = Integer.parseInt(major);
+			Integer minorInt = Integer.parseInt(minor);
+
+			if (majorUpdate) {
+				majorInt++;
+				minorInt = 0;
+			} else {
+				minorInt++;
+			}
+			versionDto.setVersion(majorInt + "." + minorInt);
 		}
-
-		Integer majorInt = Integer.parseInt(major);
-		Integer minorInt = Integer.parseInt(minor);
-
-		if (majorUpdate) {
-			majorInt++;
-			minorInt = 0;
-		} else {
-			minorInt++;
-		}
-
-		versionDto.setVersion(majorInt + "." + minorInt);
 
 		// String command = "ls -al";
 		// System.out.println("command: " + command);
